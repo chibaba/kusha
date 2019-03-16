67b6086a1d6c17efb84e59f51b08bb5debfd8da9
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-03-16 09:25:25 +0100

    create form

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -1,4 +1,6 @@
+@extends('layouts.app')
 
+@section('content')
 
 <form action="{{ route('store_blog_path') }}" method="POst">
 
@@ -7,7 +9,13 @@
 <div class="form group">
 <label for="title">Title
 </label>
-<input type="text" name="title">
+<input type="text" name="title" class="form-control">
+
 
 </div>
+<div class="form-group">
+<label for="content">Content</label>
+<textarea name="content" rows="10" class="form-control"></textarea>
+</div>
 </form>
+@endsection
