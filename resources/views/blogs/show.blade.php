28da11bcb0b54401b2f88f2190a3288b2d5e7a20
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-03-16 11:33:30 +0100

    added show page

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,1 +1,17 @@
-<h3>this is show.blade.php<h3>
+@extends('layouts.app')
+
+
+@section('content')
+
+
+<div class="row">
+
+<div class="col-md-12">
+<br>
+<h3>{{ $blog->title }}</h3>
+<p class="lead">
+ {{$blog->content}}
+</p>
+</div>
+</div>
+@endsection
